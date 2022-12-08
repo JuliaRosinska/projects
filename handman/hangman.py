6688abfe96e13d1ea9a7c7d523108fcb32d3a56e
Yuliia Rosinska <juliaharets@gmail.com>
2022-12-08 15:01:25 +0200

    Add hide_word() and unmusk_letter() funcs.
    hide_word() returns a masked word.
    unmusk_letter() returns a word with opened letters.
    Also add basic logic in main() func that waits letter from user and
    handle it with new funcs.

--- handman/hangman.py
+++ handman/hangman.py
@@ -10,6 +10,26 @@ def construct_char_dict(word):
             char_dict[letter].append(index)
     return char_dict
 
+def hide_word(word):
+    """This function returns a masked word for the player."""
+    chars = random.sample(word, k = len(word) - 2)
+
+    hide_word = word
+    for char in word:
+        if char in chars:
+            hide_word = hide_word.replace(char, "*", 1)
+            chars.remove(char)
+        else:
+            continue
+    return hide_word
+
+def unmask_letter(word_index, hide_word, letter):
+    """This function accepts entered letter and unmaskin it in the word."""
+    letter_index = word_index[letter]
+    for i in letter_index:
+        hide_word = hide_word[:i] + letter + hide_word[i + 1:]
+    return hide_word
+
 def game_status(word, attempts):
     """This func print string with actual game data."""
     print(f"{word}     |     {attempts} attempts left")
@@ -25,8 +45,25 @@ def main():
 
     word = random.choice(words)
     word_indexes = construct_char_dict(word)
+    game_word = hide_word(word)
 
     attempts = 7
-    game_status(word, attempts)
+    game_status(game_word, attempts)
+
+    end = False
+    while end == False:
+        guess = input().lower()
+        if guess in word:
+            print("Cool!")
+            game_word = unmask_letter(word_indexes, game_word, guess)
+        else:
+            if attempts == 0:
+                print("Sorry, you lost :c")
+                end = True
+
+            print("Meh..")
+            attempts -= 1
+        game_status(game_word, attempts)
+        print()
 
 main()
