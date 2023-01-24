5179b4d8e58a4847f30235cf92b4ee42bea78dd6
Yuliia Rosinska <juliaharets@gmail.com>
2023-01-24 14:11:44 +0200

    Implement removing functional.

--- reminder_bot/bot.py
+++ reminder_bot/bot.py
@@ -25,6 +25,8 @@ def check_comm(m):
         reminders_view(m)
     elif m.text == "/new":
         new_reminder(m)
+    elif m.text == "/remove":
+        reminder_remove(m)
     else:
         help(m)
 
@@ -39,6 +41,8 @@ For the new reminder send /new""", parse_mode="HTML", disable_web_page_preview=T
 
 @bot.message_handler(commands=['help'])
 def help(m):
+    """Send message to user with all available commands"""
+
     bot.send_message(m.chat.id, """<b>&#128215; List of available commands:</b>
 /new - create reminder
 /view - all reminders""", parse_mode="HTML")
@@ -47,6 +51,8 @@ def help(m):
 #   Create new reminder
 @bot.message_handler(commands=['new'])
 def new_reminder(m):
+    """Creating a new reminder with 3 steps: chossing date, enter text and time"""
+
     calendar, step = WYearTelegramCalendar().build()
     bot.send_message(m.chat.id,
                      f"Select {LSTEP[step]} &#128197;", parse_mode="HTML",
@@ -73,8 +79,11 @@ Enter message:""",
 
 @bot.message_handler(content_types = ["text"])
 def text_reminder(m):
+    """Handle text message"""
+
     global text
     text = m.text
+
     if text.startswith("/"):
         check_comm(m)
         return
@@ -83,8 +92,14 @@ def text_reminder(m):
     bot.register_next_step_handler(m, time_reminder)
 
 def time_reminder(m):
+    """Handle time"""
+
     regex = r"^[0-9]{1,2}:[0-9]{2}$"
     time = m.text
+
+    if time.startswith("/"):
+        check_comm(m)
+        return
 
     if not re.search(regex, time):
         bot.send_message(m.chat.id, "You entered incorrect time, please try again:")
@@ -112,11 +127,118 @@ def time_reminder(m):
 
 @bot.message_handler(commands=['view'])
 def reminders_view(m):
+    """Send to user a list of all reminders"""
+
     reminders = db.select_user(m.chat.id)
-    message = f"&#128203; <b>ID</b>    <b>DATE</b>          <b>REMINDER</b>\n"
-    for line in reminders:
-        message += f"&#128204; <b>{line[0]}</b>      {line[1]}:  {line[2]}\n"
-    bot.send_message(m.chat.id, message, parse_mode="HTML")
+    if db.rem_count(m.chat.id) == 0:
+        bot.send_message(m.chat.id, "You have no reminders yet.")
+    else:
+        message = f"&#128203; <b>ID</b>    <b>DATE</b>          <b>REMINDER</b>\n"
+        for line in reminders:
+            message += f"&#128204; <b>{line[0]}</b>      {line[1]}:  {line[2]}\n"
+        bot.send_message(m.chat.id, message, parse_mode="HTML")
+
+
+@bot.message_handler(commands=['remove'])
+def reminder_remove(m):
+    """Remove reminder by id_user"""
+
+    bot.send_message(m.chat.id, """If you need to remove reminders please send me a list of ids separated by a comma.
+<pre>
+1 - remove one reminders with id 1
+2,4,5 - remove reminders with id 2, 4, 5
+all - remove all your reminders
+</pre>""", parse_mode="HTML")
+    bot.register_next_step_handler(m, remove_handle)
+
+@bot.message_handler(content_types = ["text"])
+def remove_handle(m):
+    if m.text.startswith("/"):
+        check_comm(m)
+        return
+
+    text = m.text.strip()
+    id_list = []
+
+    regex = r"^([0-9]*)+(,[0-9]+)+$"
+
+    if text == "all":
+        """Handle removing all reminders."""
+        bot.send_message(m.chat.id, """Are you sure? Reminder that will be remove: all
+yes / no""")
+        bot.register_next_step_handler(m, remove_all_confirm)
+
+    elif re.search(regex, text):
+        """Handle removing some reminders."""
+        id_list = text.split(",")
+        reminders = db.select_by_id_user(id_list, m.chat.id)
+
+        global count_non_existent
+        count_non_existent = 0
+        for idx, id in enumerate(id_list):
+            if db.check_reminder(m.chat.id, id_list[idx]):
+                continue
+            else:
+                count_non_existent += 1
+                bot.send_message(m.chat.id, "Reminder with id<b> " + id + "</b> doesn't exist. Please try again.", parse_mode = "HTML")
+        if count_non_existent > 0:
+            bot.register_next_step_handler(m, remove_handle)
+            return
+
+        bot.send_message(m.chat.id, """Are you sure? Reminder that will be remove: """ + text + """
+yes / no""")
+        bot.register_next_step_handler(m, remove_some_confirm, id_list)
+
+    elif re.search(r"^([0-9]*)+$", text):
+        """Handle removing one reminder."""
+        id_list = text
+        if db.check_reminder(m.chat.id, text):
+            bot.send_message(m.chat.id, """Are you sure? Reminder that will be remove: """ + text + """
+yes / no""")
+            bot.register_next_step_handler(m, remove_some_confirm, id_list)
+        else:
+            bot.send_message(m.chat.id, f"""Reminder with id<b>{text}</b> doesn't exist. Please try again.""", parse_mode = "HTML")
+            bot.register_next_step_handler(m, remove_handle)
+
+    else:
+        bot.send_message(m.chat.id, """Incorect input. Please try again.""")
+        bot.register_next_step_handler(m, remove_handle)
+
+
+@bot.message_handler(content_types = ["text"])
+def remove_some_confirm(m, id_list):
+    if m.text.startswith("/"):
+        check_comm(m)
+        return
+
+    confirm = m.text
+    if confirm == "yes":
+        for idx, id in enumerate(id_list):
+            db.delete_reminder(m.chat.id, id_list[idx])
+        bot.send_message(m.chat.id, "Removing access!")
+
+    elif confirm == "no":
+        bot.send_message(m.chat.id, "Okey, removing was canceled.")
+    else:
+        bot.send_message(m.chat.id, "Sorry, I can't understand. Please try again.")
+        bot.register_next_step_handler(m, remove_all_confirm)
+
+def remove_all_confirm(m):
+    if m.text.startswith("/"):
+        check_comm(m)
+        return
+
+    confirm = m.text
+    if confirm == "yes":
+        db.delete_all_reminders(m.chat.id)
+        bot.send_message(m.chat.id, "Removing access!")
+    elif confirm == "no":
+        bot.send_message(m.chat.id, "Okey, removing was canceled.")
+    else:
+        bot.send_message(chat.id.m, "Sorry, I can't understand. Please try again.")
+        bot.register_next_step_handler(m, remove_all_confirm)
+
+
 
 
 
@@ -136,6 +258,7 @@ def reminder_check():
 
 schedule.every().hour.at(":30").do(reminder_check)
 schedule.every().hour.at(":00").do(reminder_check)
+
 
 def schedule_run():
     while True:
--- reminder_bot/db.py
+++ reminder_bot/db.py
@@ -38,10 +38,37 @@ def select_user(chat_id):
     result = cursor.fetchall()
     return result
 
+def select_by_id_user(id_list, chat_id):
+    """Select reminders by id_user"""
 
-def delete_reminder(id):
+    id_str = ", ".join(id_list)
+    cursor.execute(f"""SELECT * FROM reminders WHERE chat_id = {chat_id} AND id_user IN ({id_str})""")
+    result = cursor.fetchall()
+    return result
+
+def check_reminder(chat_id, id_user):
+    """Check if reminder exist in table. Return boolean."""
+
+    cursor.execute(f"""SELECT * FROM reminders WHERE chat_id = {chat_id} AND id_user = {id_user}""")
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return False
+    else:
+        return True
+
+def update_id(chat_id):
+    """Rewrite all id_user after removin. Renumbers the list of reminders."""
+    cursor.execute(f"""UPDATE reminders SET id_user = (SELECT new_id FROM (SELECT id, ROW_NUMBER () OVER() as new_id FROM reminders WHERE chat_id = 371971695)) WHERE chat_id = 371971695;""")
+
+
+def delete_reminder(chat_id, id_user):
     """Delet reminder by id."""
 
-    cursor.execute(f"DELETE FROM reminders WHERE id = {id}")
+    cursor.execute(f"""DELETE FROM reminders WHERE chat_id = {chat_id} AND id_user = {id_user}""")
     db.commit()
 
+def delete_all_reminders(chat_id):
+    """Delete all users reminders"""
+
+    cursor.execute(f"""DELETE FROM reminders WHERE chat_id = {chat_id}""")
+    db.commit()
